7b1fbb331eb4b328eb9e9d2421678f74cef616b3
hrishi <hrishikesh.mk1@gmail.com>
2020-06-15 20:29:31 +0530

    data test

---
+++ main.js
@@ -0,0 +1,18 @@
+const getData = (url) => {
+    return fetch(url);
+};
+
+const getQuality30 = async () => {
+    const url = './data/MW-NIFTY200-QUALITY.csv';
+    const data = await getData(url);
+    console.log(data);
+};
+
+const getLowVolatile = () => {
+    const url = './data/MW-NIFTY100-LOW-VOLATILITY.csv';
+    const data = await getData(url);
+    console.log(data);
+};
+
+getQuality30();
+getLowVolatile();
