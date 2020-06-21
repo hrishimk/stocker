d058db18b73821d659d48dc30398518f69cd338d
hrishi <hrishikesh.mk1@gmail.com>
2020-06-22 00:16:09 +0530

    Updates

---
+++ precache-manifest.e6accead6bdd51a2f007eb3af0f8922c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b659f407496910724828fb6170f0075e",
+    "url": "/stocker/index.html"
+  },
+  {
+    "revision": "26925c6cf0849ea9dceb",
+    "url": "/stocker/static/css/main.0b9a383e.chunk.css"
+  },
+  {
+    "revision": "deed8c545c17d8b874c2",
+    "url": "/stocker/static/js/2.81008dba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stocker/static/js/2.81008dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26925c6cf0849ea9dceb",
+    "url": "/stocker/static/js/main.ee18da9e.chunk.js"
+  },
+  {
+    "revision": "38fba56d99fb2723c220",
+    "url": "/stocker/static/js/runtime-main.7208ab21.js"
+  }
+]);
